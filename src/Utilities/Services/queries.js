adc9902db521f5d92d8159223cebe973dde22ab4
khashayar <khashayarmobarezbusiness@gmail.com>
2024-02-21 09:42:43 +0330

    merging branches

--- src/Utilities/Services/queries.js
+++ src/Utilities/Services/queries.js
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from 'axios';
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com/photos'
+
 const useUserDetails = () => {
-    return useQuery(['user'], () => axios.get(`https://jsonplaceholder.typicode.com/photos/5`));
+    return useQuery(['user'], () => axios.get(`${BASE_URL}/5`));
 };
 
 export { useUserDetails };
