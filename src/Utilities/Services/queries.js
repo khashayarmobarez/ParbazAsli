07a4705fe07a4d5abe56a2620f772b31620751fe
khashayar <khashayarmobarezbusiness@gmail.com>
2024-05-11 22:10:31 +0330

    api data

--- src/Utilities/Services/queries.js
+++ src/Utilities/Services/queries.js
@@ -2,10 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import axios from 'axios';
 
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com/photos'
+const BASE_Test_URL = 'https://jsonplaceholder.typicode.com/photos'
+const BASE_URL = 'http://195.177.255.109/api'
 
 const useUserDetails = () => {
-    return useQuery(['user'], () => axios.get(`${BASE_URL}/5`));
+    return useQuery(['user'], () => axios.get(`${BASE_Test_URL}/5`));
 };
 
-export { useUserDetails };
+const useLandingPage = () => {
+    return useQuery(['landing'], () => axios.get(`${BASE_URL}/Landing/GetHomeSections`));
+  };
+  
+
+export { useUserDetails, useLandingPage };
