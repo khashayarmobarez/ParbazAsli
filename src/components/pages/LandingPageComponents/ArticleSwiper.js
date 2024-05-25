1831258159a5a5c848f8e83f8264342cb241c787
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-05-25 16:18:02 +0330

    finished crousel

--- src/components/pages/LandingPageComponents/ArticleSwiper.js
+++ src/components/pages/LandingPageComponents/ArticleSwiper.js
@@ -10,6 +10,7 @@ import { Navigation, Pagination } from 'swiper/modules';
 
 // mui
 import useMediaQuery from '@mui/material/useMediaQuery';
+import WatchLaterOutlinedIcon from '@mui/icons-material/WatchLaterOutlined';
 
 // styles
 import styles from './ArticleSwiper.module.css';
@@ -52,7 +53,7 @@ import { useBlogs } from '../../../Utilities/Services/queries';
   }
 
   return (
-    <div className='w-full md:pl-[8%] md:pr-[10%]'>
+    <div className='w-full md:pl-[8%] md:pr-[11%]'>
       <Swiper
         modules={[Navigation, Pagination]}
         navigation={{
@@ -64,32 +65,68 @@ import { useBlogs } from '../../../Utilities/Services/queries';
           bulletClass: styles.swiperPaginationBullet,
         }}
         centeredSlides={!isDesktop}
-        spaceBetween={isDesktop ? 50 : 0}
+        spaceBetween={isDesktop ? 60 : 0}
         slidesPerView={isDesktop ? 3 : 1}
         initialSlide={0}
         onSlideChange={handleSlideChange} 
-        autoplay={{ delay: 4000 }}
+        autoplay={{ delay: 3000 }}
       >
 
         {blogData.data.map((blog,index) => (
           <SwiperSlide
             key={blog.id}
-            className={`${styles.swiperSlide} ${index === activeSlideIndex  ? styles.activeSlide : ''}`} // Apply active slide style conditionally
+            className={`${styles.swiperSlide} `} // Apply active slide style conditionally
             style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}
           >
 
-            <div className={`${styles.productCard} `}>
+            <div className={`${styles.productCard} ${index === activeSlideIndex  ? 'w-full' : 'md:w-[85%] md:mt-12 w-[75%]'} `}>
 
-              <img src={blog.image.path} alt={blog.title} className={styles.productImage} />
-              <h3 className=' text-xl' style={{color:'var(--yellow-text)'}}>{blog.title}</h3>
-              <p className=' mb-4' style={{color:'var(--softer-white)'}}>{blog.authorName}</p>
-              <p className=' mb-8' style={{color:'var(--softer-white)'}}>
-                { blog.blogSections && blog.blogSections.length > 0 && blog.blogSections[0].htmlContent
-                  ? extractWordsFromHtml(blog.blogSections[0].htmlContent, 30)
-                  : 'No content available'
-                } ...
-              </p>
-              <button onClick={() => navigate(`/blog/${blog.id}`)} className={`${ButtonStyles.addButton} w-36 `}>خواندن مقاله</button>
+            {/* conditions to set active styling for desktop mode */}
+            {
+              isDesktop ? 
+                ( index === activeSlideIndex &&
+                  <p className='absolute top-3 py-1 px-2 text-sm self-end z-30 rounded-r-3xl' style={{background:'var(--dark-blue-bg)'}} >
+                    <WatchLaterOutlinedIcon  /> در {blog.timeToReadInMinutes} دقیقه بخوانید
+                  </p>
+                )
+                :
+                <p className='absolute top-3 py-1 px-2 text-sm self-end z-30 rounded-r-3xl' style={{background:'var(--dark-blue-bg)'}} >
+                    <WatchLaterOutlinedIcon  /> در {blog.timeToReadInMinutes} دقیقه بخوانید
+                </p>
+            }
+
+              <img src={blog.image.path} alt={blog.title} className={` w-full  object-cover mb-4 z-10 ${index === activeSlideIndex  ? ' h-72' : 'h-56'}`} />
+
+              {/* conditions to set active styling for desktop mode */}
+              {
+                isDesktop ? 
+                  ( index === activeSlideIndex &&
+                  <>
+                    <h3 className=' text-xl' style={{color:'var(--yellow-text)'}}>{blog.title}</h3>
+                    <p className=' mb-4' style={{color:'var(--softer-white)'}}>{blog.authorName}</p>
+                    <p className=' mb-8' style={{color:'var(--softer-white)'}}>
+                      { blog.blogSections && blog.blogSections.length > 0 && blog.blogSections[0].htmlContent
+                        ? extractWordsFromHtml(blog.blogSections[0].htmlContent, 30)
+                        : 'No content available'
+                      } ...
+                    </p>
+                    <button onClick={() => navigate(`/blog/${blog.id}`)} className={`${ButtonStyles.addButton} w-36 `}>خواندن مقاله</button>
+                  </>
+                  ) 
+                  :
+                  <>
+                    <h3 className=' text-xl' style={{color:'var(--yellow-text)'}}>{blog.title}</h3>
+                    <p className=' mb-4' style={{color:'var(--softer-white)'}}>{blog.authorName}</p>
+                    <p className=' mb-8' style={{color:'var(--softer-white)'}}>
+                      { blog.blogSections && blog.blogSections.length > 0 && blog.blogSections[0].htmlContent
+                        ? extractWordsFromHtml(blog.blogSections[0].htmlContent, 30)
+                        : 'No content available'
+                      } ...
+                    </p>
+                    <button onClick={() => navigate(`/blog/${blog.id}`)} className={`${ButtonStyles.addButton} w-36 `}>خواندن مقاله</button>
+                  </>
+              }
+
             </div>
 
           </SwiperSlide>
