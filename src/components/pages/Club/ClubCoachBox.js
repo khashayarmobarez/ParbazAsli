f37e818072e1590b8f3525a3aa7c0636416e3255
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-07-17 15:13:40 +0330

    refactor: Enable coach details navigation in ClubCoachBox component

--- src/components/pages/Club/ClubCoachBox.js
+++ src/components/pages/Club/ClubCoachBox.js
@@ -8,14 +8,14 @@ const ClubCoachBox = ({ coachData }) => {
 
     const navigate = useNavigate();
 
-    // const clickHandler = (id) => () => {
-    //     navigate(`/club/coachDetails/${id}`)
-    // }
+    const clickHandler = (id) => () => {
+        navigate(`/club/coachDetails/${id}`)
+    }
 
     return (
         <>
             <div
-            // onClick={(coachData.status === 'Active' || coachData.status === 'Disable') && clickHandler(coachData.id)} 
+            onClick={(coachData.status === 'Active' || coachData.status === 'Disable') && clickHandler(coachData.id)} 
             className='flex w-full justify-between items-center rounded-2xl text-xs h-16 px-2' 
             style={{background:'var(--coachesDetails-bg)', boxShadow:'var(--coachesDetails-BoxShadow)'}} >
                 <Avatar alt="Remy Sharp" sx={{height:'40px', width:'40px', zIndex:'0'}} />
