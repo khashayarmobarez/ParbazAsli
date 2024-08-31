0892a4245eecb30d84c9e781666ac886ca2a0c3a
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-08-31 12:07:09 +0330

    removed mui/styles

--- src/components/inputs/TimeInput.js
+++ src/components/inputs/TimeInput.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@mui/styles';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -8,64 +6,9 @@ import { TextField } from '@mui/material';
 import dayjs from 'dayjs';
 import clockIcon from '../../assets/icons/flightHour.svg';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    minHeight: '2rem',
-    borderRadius: '2xl',
-    position: 'relative',
-    display: 'flex',
-  },
-  icon: {
-    position: 'absolute',
-    width: '1.5rem',
-    marginTop: '12px',
-    marginRight: '8px',
-    zIndex: 10,
-    fill: 'white',
-  },
-  timePicker: {
-    width: '100%',
-    height: '3rem',
-    background: 'linear-gradient(0deg, #181A2D, #181A2D), linear-gradient(215.85deg, rgba(238, 238, 238, 0.46) -45.31%, rgba(238, 238, 238, 0) 168.95%)',
-    border: '1px solid rgba(255, 255, 255, 0.3)',
-    boxShadow: '-3px 4px 4px 1px rgba(0, 0, 0, 0.32)',
-    appearance: 'none',
-    borderRadius: '0.75rem',
-    '& .MuiInputBase-input': {
-      color: 'var(--softer-white)',
-    },
-    '& .MuiInputBase-root': {
-      paddingRight: '2.5rem',
-      borderRadius: '0.75rem',
-      height: '3rem',
-      border: '1px solid rgba(255, 255, 255, 0.5)',
-      '&:hover': {
-        background: 'none',
-      },
-      '&.Mui-focused': {
-        background: 'none',
-        boxShadow: 'none',
-        border: '1px solid var(--low-opacity-white)',
-      },
-    },
-  },
-  textField: {
-    width: '100%',
-    '& .MuiInput-underline:before': {
-      borderBottomColor: 'rgba(255, 255, 255, 0.7)',
-    },
-    '& .MuiInputBase-input': {
-      '&::placeholder': {
-        color: 'rgba(255, 255, 255, 0.5)',
-        opacity: 1,
-      },
-    },
-  },
-}));
+
 
 const TimeInput = ({ value, onChange, placeholder, icon }) => {
-  const classes = useStyles();
   const [selectedTime, setSelectedTime] = useState(dayjs(value));
 
   const handleTimeChange = (newValue) => {
@@ -74,9 +17,9 @@ const TimeInput = ({ value, onChange, placeholder, icon }) => {
   };
 
   return (
-    <div className={classes.root}>
+    <div className={`w-full min-h-8 rounded-2xl relative flex`}>
       <span>
-        <img src={clockIcon} alt='icon' className={classes.icon} />
+        <img src={clockIcon} alt='icon' className={`absolute w-6 mt-3 mr-2 z-10`} />
       </span>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <MobileTimePicker
@@ -91,11 +34,47 @@ const TimeInput = ({ value, onChange, placeholder, icon }) => {
                 ...params.InputProps,
                 placeholder: placeholder || "hh:mm",
               }}
-              className={classes.textField}
+              sx={{
+                  width: '100%',
+                  '& .MuiInput-underline:before': {
+                    borderBottomColor: 'rgba(255, 255, 255, 0.7)',
+                  },
+                  '& .MuiInputBase-input': {
+                    '&::placeholder': {
+                      color: 'rgba(255, 255, 255, 0.5)',
+                      opacity: 1,
+                    },
+                  },
+              }}
               label="گزینه‌ی‌زمان"
             />
           )}
-          className={classes.timePicker}
+          sx={{
+            width: '100%',
+            height: '3rem',
+            background: 'linear-gradient(0deg, #181A2D, #181A2D), linear-gradient(215.85deg, rgba(238, 238, 238, 0.46) -45.31%, rgba(238, 238, 238, 0) 168.95%)',
+            border: '1px solid rgba(255, 255, 255, 0.3)',
+            boxShadow: '-3px 4px 4px 1px rgba(0, 0, 0, 0.32)',
+            appearance: 'none',
+            borderRadius: '0.75rem',
+            '& .MuiInputBase-input': {
+              color: 'var(--softer-white)',
+            },
+            '& .MuiInputBase-root': {
+              paddingRight: '2.5rem',
+              borderRadius: '0.75rem',
+              height: '3rem',
+              border: '1px solid rgba(255, 255, 255, 0.5)',
+              '&:hover': {
+                background: 'none',
+              },
+              '&.Mui-focused': {
+                background: 'none',
+                boxShadow: 'none',
+                border: '1px solid var(--low-opacity-white)',
+              },
+            },
+          }}
         />
       </LocalizationProvider>
     </div>
