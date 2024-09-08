e2ec02324bbbcd766537c49f22b035b8ca6c7ecf
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-09-08 14:26:25 +0330

    developed survey pages

--- src/components/pages/Notifications/TandemSurvey.js
+++ src/components/pages/Notifications/TandemSurvey.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 // styles
 import styles from './TandemSurvey.module.css';
@@ -7,6 +7,10 @@ import ButtonStyles from '../../../styles/Buttons/ButtonsBox.module.css'
 
 // mui
 import { CircularProgress } from '@mui/material';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+// assets
+import checkIcon from '../../../assets/icons/bigCheckIcon.svg'
 
 // queries
 import { useIsSurveyAvailabe, useSubmitSurvey } from '../../../Utilities/Services/notificationAndSurveyQueries';
@@ -15,11 +19,15 @@ import { toast } from 'react-toastify';
 import StarRating from '../../reuseable/StarRating';
 
 const TandemSurvey = () => {
+
+    const navigate = useNavigate()
     const { id } = useParams();
 
     const [rating, setRating] = useState(0); // Default to 2 stars
     const [description, setDescription] = useState('');
     const [videoFiles, setVideoFiles] = useState([]);
+
+    const [ surveyIsSubmited, setSurveyIsSubmited ] = useState(false)
 
     const { data: IsSurveyAvailable, isLoading: availablityLoading, error: availablityError } = useIsSurveyAvailabe(id);
     const { mutate: submitSurvey, isLoading: submitSubmitSurveyLoading } = useSubmitSurvey();
@@ -36,7 +44,7 @@ const TandemSurvey = () => {
         const validFiles = files.filter((file) => validTypes.includes(file.type) && file.size <= maxSize);
     
         if (validFiles.length > 0) {
-          setVideoFiles(validFiles);
+            setVideoFiles((prevFiles) => [...prevFiles, ...validFiles]);
         } else {
           setVideoFiles([]);
           toast('نوع یا اندازه فایل‌ها نامعتبر است. لطفاً فایل‌های ویدیویی کمتر از 50 مگابایت آپلود کنید.', {
@@ -47,7 +55,11 @@ const TandemSurvey = () => {
             style: { width: '90%' },
           });
         }
-      };
+    };
+
+    const handleRemoveVideo = (file) => {
+        setVideoFiles((prevFiles) => prevFiles.filter((f) => f !== file));
+    };
 
 
 
@@ -66,6 +78,8 @@ const TandemSurvey = () => {
         } else {
 
             const formData = new FormData();
+
+            console.log()
 
             videoFiles.forEach((file) => {
                 formData.append(`files`, file); // Append each file with a unique key
@@ -86,7 +100,7 @@ const TandemSurvey = () => {
                         });
                         setTimeout(() => {
                             window.location.reload();
-                        }, 3000);
+                        }, 1000);
                     },
                     onError: (error) => {
                         let errorMessage = 'خطایی رخ داده است';
@@ -113,79 +127,95 @@ const TandemSurvey = () => {
 
     return (
         <div className='w-full flex flex-col items-center'>
-            <div className='flex flex-col py-16 justify-center items-center w-[90%] md:w-[80%]'>
+                    <div className='flex flex-col py-16 justify-center items-center w-[90%] md:w-[80%]'>
 
-                {
-                    availablityLoading &&
-                        <div className='fixed w-[100svh] h-[100svh] z-[110] backdrop-blur-sm flex flex-col justify-center items-center gap-y-2 pt-56'>
-                            <CircularProgress sx={{ color:'var(--yellow-text) '}} />
-                        </div>
-                }
+                        {
+                            availablityLoading &&
+                                <div className='fixed w-[100svh] h-[100svh] z-[110] backdrop-blur-sm flex flex-col justify-center items-center gap-y-2 pt-56'>
+                                    <CircularProgress sx={{ color:'var(--yellow-text) '}} />
+                                </div>
+                        }
 
-                {
-                    !availablityLoading && !IsSurveyAvailable &&
-                    <p className='mt-32' style={{ color: 'var(--yellow-text)' }}>این نظرسنجی ثبت شده است</p>
-                }
+                        {
+                            !availablityLoading && !IsSurveyAvailable &&
+                            <div className='mt-32 flex flex-col items-center justify-center gap-y-6 w-full'>
+                                <img src={checkIcon} alt='check icon' className='w-32 h-32' />
+                                <p className='' style={{ color: 'var(--yellow-text)' }}>با تشکر از همکاری شما...</p>
+                                <button onClick={() => navigate('/profile')} type="submit" className={`${ButtonStyles.addButton} w-32 h-12`}>صفحه اصلی</button>
+                            </div>
+                        }
 
-                {
-                    IsSurveyAvailable &&
-                    <form className='w-full flex flex-col gap-y-4 items-center pt-16' onSubmit={handleSubmit}>
+                        {
+                            IsSurveyAvailable &&
+                            <form className='w-full flex flex-col gap-y-4 items-center pt-16' onSubmit={handleSubmit}>
 
-                        <p className='text-center px-4 text-sm'>با نظرات مفید خود از پروازی که انجام دادید ما را در راستای پیشرفت و هر چه بهتر شدن یاری فرمایید</p>
+                                <p className='text-center px-4 text-sm'>با نظرات مفید خود از پروازی که انجام دادید ما را در راستای پیشرفت و هر چه بهتر شدن یاری فرمایید</p>
 
-                        {/* star rating */}
-                        <div className='w-[70%]'>
-                            <StarRating rate={rating} setRate={setRating} />
-                        </div>
+                                {/* star rating */}
+                                <div className='w-[70%]'>
+                                    <StarRating rate={rating} setRate={setRating} />
+                                </div>
 
-                        {/* description input */}
-                        <div className='w-full flex flex-col gap-y-2 '>
-                            <DescriptionInput
-                                value={description}
-                                onChange={handleDescription}
-                                placeholder='دلیل نمره خود را بنویسید...'
-                            />
-                        </div>
+                                {/* description input */}
+                                <div className='w-full flex flex-col gap-y-2 '>
+                                    <DescriptionInput
+                                        value={description}
+                                        onChange={handleDescription}
+                                        placeholder='دلیل نمره خود را بنویسید...'
+                                    />
+                                </div>
 
-                        <p className='text-center px-4 text-base mt-6 mb-2 text-[var(--yellow-text)]'>در صورت تمایل ویدیو پرواز خود را آپلود کنید</p>
+                                <p className='text-center px-4 text-base mt-6 mb-2 text-[var(--yellow-text)]'>در صورت تمایل ویدیو پرواز خود را آپلود کنید</p>
 
-                        
-                        <div className="w-full">
+                                
+                                <div className="w-full">
 
-                            {/* Hidden file input */}
-                            <input
-                                id="fileInput"
-                                className="hidden" // Hide the default input
-                                name="file"
-                                type="file"
-                                onChange={handleFileChange}
-                            />
+                                    {/* Hidden file input */}
+                                    <input
+                                        id="fileInput"
+                                        className="hidden" // Hide the default input
+                                        name="file"
+                                        type="file"
+                                        onChange={handleFileChange}
+                                        multiple
+                                    />
 
-                            {/* Custom-styled label to act as a button */}
-                            <label
-                                htmlFor="fileInput"
-                                className={`${ButtonStyles.normalButton} w-full h-12 flex items-center justify-center cursor-pointer`} // Custom styles
-                            >
-                                آپلود  ویدئو
-                            </label>
+                                    {/* Custom-styled label to act as a button */}
+                                    <label
+                                        htmlFor="fileInput"
+                                        className={`${ButtonStyles.normalButton} w-full h-12 flex items-center justify-center cursor-pointer`} // Custom styles
+                                    >
+                                        آپلود  ویدئو
+                                    </label>
 
-                            <p className=' text-xs mt-2 text-start'>فرمت مجاز: mp4, mov ,webm و mkv تا حجم 4 مگابایت</p>
+                                    <p className=' text-xs mt-2 text-start'>فرمت مجاز: mp4, mov ,webm و mkv تا حجم 4 مگابایت</p>
 
-                        </div>
+                                </div>
 
-                        {videoFiles.length > 0 && (
-                            <ul className="text-xs mt-2">
-                                {videoFiles.map((file, index) => (
-                                <li key={index}>{file.name}</li>
-                                ))}
-                            </ul>
-                        )}
+                                {videoFiles.length > 0 && (
+                                    <ul className="w-full text-xs mt-2">
+                                        {videoFiles.map((file, index) => (
+                                            <li key={index} className='w-full px-4 py-3 gap-x-4 rounded-2xl flex justify-between items-center mt-4'
+                                            style={{background:  'var(--profile-buttons-background)',
+                                                boxShadow: 'var(--profile-buttons-boxShadow)'}}>
+                                                    <p>{index + 1}</p>
+                                                    <p>{file.name}</p> 
+                                                    <RemoveIcon sx={{background:  'var(--profile-buttons-background)',
+                                                    boxShadow: 'var(--profile-buttons-boxShadow)',
+                                                    borderRadius:'0.5rem',
+                                                    color:'var(--red-text)'}}
+                                                    onClick={() => handleRemoveVideo(file)}
+                                                    />
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
 
-                        <button type="submit" disabled={!rating || submitSubmitSurveyLoading} className={`${ButtonStyles.addButton} ${(!rating || submitSubmitSurveyLoading) && 'opacity-45'} w-32 h-12 mt-8`}>تایید</button>
+                                <button type="submit" disabled={!rating || submitSubmitSurveyLoading} className={`${ButtonStyles.addButton} ${(!rating || submitSubmitSurveyLoading) && 'opacity-45'} w-32 h-12 mt-8`}>تایید</button>
 
-                    </form>
-                }
-            </div>
+                            </form>
+                        }
+                    </div>
         </div>
     );
 };
