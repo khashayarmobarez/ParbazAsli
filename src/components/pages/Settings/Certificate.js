53651695b7f7f5722181de185b33267e15de881e
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-09-09 12:49:31 +0330

    Refactor Certificate component to improve UI and display certificate status and rejection reason if applicable

--- src/components/pages/Settings/Certificate.js
+++ src/components/pages/Settings/Certificate.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router';
-
-import ButtonStyles from '../../../styles/Buttons/ButtonsBox.module.css'
 
 //  assets
 import certifiacte from '../../../assets/icons/certificate-Vector.svg'
@@ -9,23 +6,34 @@ import certifiacte from '../../../assets/icons/certificate-Vector.svg'
 
 const Certificate = ({certificateData}) => {
 
-    // react-router-dom
-    const navigate = useNavigate()
 
     return (
-        <div className=' w-full h-16 rounded-full flex items-center justify-between px-4' style={{background:'var(--about-us-box-color)', boxShadow:'var(--about-us-box-shodow)', color:'var(--soft-white) ' }}>
+        <div className=' w-full min-h-16 rounded-[2rem] flex flex-col justify-between p-4 ' style={{background:'var(--organs-coachData-bg)', boxShadow:'var(--organs-coachData-boxShadow)', color:'var(--soft-white) ' }}>
             
-            <div className=' w-full text-xs flex justify-between items-center'>
+            <div className=' w-full text-xs flex justify-between items-center gap-x-4'>
 
-                <div className=' flex'>
+                <div className=' flex gap-x-2 '>
                     <img src={certifiacte} alt='icon' />
-                    <p>{certificateData.organization} / {certificateData.level}</p>
+                    <p className='flex-wrap' >{certificateData.organization} / {certificateData.level}</p>
                 </div>
-                <div>
+                <div className='flex flex-col items-start gap-y-2'>
+                    <p className='text-[var(--low-opacity-white)] '>
+                        وضعیت:
+                        {certificateData.status === 'Active' && <span style={{color:'var(--yellow-text)'}}> فعال</span>}
+                        {certificateData.status === 'Pending' && <span style={{color:'var(--primary-light)'}}> در انتظار تایید</span>}
+                        {certificateData.status === 'Expired' && <span style={{color:'var(--red-text)'}}> منقضی شده</span>}
+                        {certificateData.status === 'Rejected' && <span style={{color:'var(--notification-red)'}}> رد شده</span>}
+                    </p>
                     <p>تاریخ {certificateData.expirationDate}</p>
                 </div>
 
             </div>
+
+            {certificateData.status === 'Rejected' &&
+                <div className='w-full flex items-center gap-x-2  mt-4'>
+                    <p className='text-sm text-[var(--notification-red)]'>دلیل رد شدن: {certificateData.rejectionReason}</p>
+                </div>
+            }
 
         </div>
     );
