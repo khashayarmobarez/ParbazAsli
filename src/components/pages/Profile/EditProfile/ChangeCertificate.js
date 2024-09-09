ab49207d5e1d231876a087519fb7f5154de552ea
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-09-09 12:56:57 +0330

    Refactor ChangeCertificate component to handle pagination and add navigation buttons

--- src/components/pages/Profile/EditProfile/ChangeCertificate.js
+++ src/components/pages/Profile/EditProfile/ChangeCertificate.js
@@ -7,7 +7,8 @@ import ButtonStyles from '../../../../styles/Buttons/ButtonsBox.module.css'
 // queries
 import { useAllUserCertificates } from '../../../../Utilities/Services/userQueries';
 
-// mui 
+// mui and assets
+import arrowIcon from '../../../../assets/icons/Right Arrow Button.svg';
 import AddIcon from '@mui/icons-material/Add';
 
 // components
@@ -16,28 +17,59 @@ import Certificate from '../../Settings/Certificate';
 
 const ChangeCertificate = () => {
 
-    const [pageSize, setPageSize] = useState(5)
+    const [pageNumber, setPageNumber] = useState(1)
 
-    const { data: userCertificates, isLoading, error } = useAllUserCertificates(1,pageSize);
+    const { data: userCertificates, isLoading, error } = useAllUserCertificates(pageNumber,5);
+
+    const handleNextPageNumber = () => {
+        setPageNumber(prev => prev + 1)
+    }
+
+    const handleLastPageNumber = () => {
+        setPageNumber(prev => prev - 1)
+    }
 
 
     return (
             <div className='w-[90%] flex flex-col items-center gap-y-6 mt-4'>
                 {
-                    userCertificates && userCertificates.data.map((certificate) => (
+                        userCertificates && userCertificates.data.map((certificate) => (
 
-                        <Certificate certificateData={certificate} />
+                            <Certificate certificateData={certificate} />
 
-                    ))
+                        ))
                 }
 
-                {   
-                    userCertificates && userCertificates.totalCount >= pageSize &&
-                    <p 
-                    onClick={() => setPageSize(pageSize + 5)}
-                    className='w-full' style={{color:'var(--yellow-text)'}}>
-                        مشاهده بیشتر ...
-                    </p>
+                {userCertificates && userCertificates.totalPagesCount > 1 &&
+                    <div className='w-full flex justify-between px-14 items-center mt-2'>
+                        <button
+                            className='transform  w-10 justify-self-end'
+                            disabled={pageNumber === 1}
+                            onClick={handleLastPageNumber}
+                        >
+                            <img
+                                src={arrowIcon}
+                                alt='arrow'
+                                className={`mt-2 ${pageNumber === 1 && 'opacity-60'}`}
+                            />
+                        </button>
+
+                        <p className='text-sm justify-self-center' style={{ color: 'var(--yellow-text)' }}>
+                            صفحه ی {pageNumber}
+                        </p>
+
+                        <button
+                            className='w-10 rotate-180 justify-self-start'
+                            disabled={userCertificates.totalPagesCount === 1 || userCertificates.totalPagesCount === pageNumber}
+                            onClick={handleNextPageNumber}
+                        >
+                            <img
+                                src={arrowIcon}
+                                alt='arrow'
+                                className={`${(userCertificates.totalPagesCount === 1 || userCertificates.totalPagesCount === pageNumber) && 'opacity-60'}`}
+                            />
+                        </button>
+                    </div>
                 }
 
                 <Link to='/Settings/certificate' className='fixed bottom-[4.5rem] w-[90%] bg-[#131423] rounded-xl md:w-96 md:relative md:bottom-0 md:top-4  '>
