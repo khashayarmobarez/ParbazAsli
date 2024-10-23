2e9b8f680d6dc88df1f77224112f506231d6963a
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-10-23 19:08:54 +0330

    Refactor Checkbox component to conditionally render label based on the presence of 'noLabel' prop

--- src/components/pages/authentication/Inputs/CheckBox.js
+++ src/components/pages/authentication/Inputs/CheckBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 import { useNavigate } from 'react-router-dom';
 
-const Checkbox = ({ label, isChecked, onToggle, openTerms }) => {
+const Checkbox = ({ label, isChecked, onToggle, openTerms, noLabel }) => {
 
   const toggleCheckbox = () => {
     onToggle(!isChecked); // Pass the new checked state to the parent component
@@ -13,7 +13,10 @@ const Checkbox = ({ label, isChecked, onToggle, openTerms }) => {
       <div className={`w-6 h-6 border-2 rounded flex items-center justify-center cursor-pointer`} style={{borderColor:'var(--text-accent)'}}>
         {isChecked && < CheckRoundedIcon sx={{color: 'var(--text-accent)'}} />}
       </div>
-      <div className=" underline underline-offset-4 cursor-pointer" >{label}</div>
+      {
+        !noLabel &&
+        <div className=" underline underline-offset-4 cursor-pointer" >{label}</div>
+      }
     </div>
   );
 };
