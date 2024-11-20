880bbd0c75f60bfd5471b47365c5d24d7c1cdae6
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-11-20 18:04:09 +0330

    Add responsive background images and logo to SignUpOrLogin component

--- src/components/pages/authentication/SignUpOrLogin.js
+++ src/components/pages/authentication/SignUpOrLogin.js
@@ -8,14 +8,28 @@ import SignUp from './SignUp';
 import Login from './Login';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+// assets
+import authenticationBackground from '../../../assets/image/logInBackgroundReduced.png'
+import logo from '../../../assets/Logo/DigilogbookNoFeatherLogo.png'
+import { useMediaQuery } from '@mui/material';
+
 const SignUpOrLogin = () => {
+
+    const isDesktop = useMediaQuery('(min-width: 768px)');
 
     const navigate = useNavigate();
     const location = useLocation();
 
     return (
-        <div className='pt-12 flex flex-col items-center'>
-            <div className='w-full mt-6 flex flex-col items-center gap-y-7 justify-center md:w-[55%]'>
+        <div className='pt-0 flex flex-col items-center md:flex-row-reverse bg-bgPageMain h-full'>
+            {
+                isDesktop &&
+                <div className='w-[90%] h-[100dvh] relative'>
+                    <img src={authenticationBackground} alt='Authentication' className='w-full h-full object-cover' />
+                    <img src={logo} alt='logo' className='w-80 absolute left-[34%] top-[42dvh]' />
+                </div>
+            }
+            <div className='w-full mt-16 flex flex-col items-center gap-y-7 justify-center md:w-[40%] md:px-10'>
 
                 <div className={`${ButtonStyles.ThreeStickedButtonCont}`}>
                     <button 
