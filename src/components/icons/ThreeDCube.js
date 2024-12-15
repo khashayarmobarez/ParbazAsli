35fb97ef3f2bd1b9529bc2be67cce9477ea23a83
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-12-15 14:36:18 +0330

    fixed some bugs

--- src/components/icons/ThreeDCube.js
+++ src/components/icons/ThreeDCube.js
@@ -5,7 +5,7 @@ const ThreeDCube = () => {
         <svg width="100%" height="100%" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M16.6667 5.83333L10 2.5L3.33337 5.83333M16.6667 5.83333L10 9.16667M16.6667 5.83333V14.1667L10 17.5M10 9.16667L3.33337 5.83333M10 9.16667L10 17.5M3.33337 5.83333L3.33337 14.1667L10 17.5" 
             stroke="var(--text-default)" 
-            stroke-linejoin="round"/>
+            strokeLinejoin="round"/>
         </svg>
     );
 };
